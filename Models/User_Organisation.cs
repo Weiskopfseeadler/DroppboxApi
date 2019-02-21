4194ff2eef44d76a2e7ff657c11f4151863db8da
Weiskopfseeadler <patrick.grunder@zapp.ch>
2019-02-21 11:49:29 +0100

    Api fast fertig

--- Models/User_Organisation.cs
+++ Models/User_Organisation.cs
@@ -2,13 +2,13 @@ using System;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DroppboxApi.Models
-{
+{    
     public class User_Organisation
     {
-        public long id{get;set;}
+        public long id {get;set;}
         [ForeignKey("User")]
-        public long userId{get;set;}
+        public long userId {get;set;}
         [ForeignKey("Organization")]
-        public long organisationId{get;set;}
+        public long organisationId {get;set;}
     }
 }
